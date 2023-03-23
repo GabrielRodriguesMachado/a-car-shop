a6bd0b5c462fdae0e6c645a084e20513716a695f
GabrielRodriguesMachado <gabrm123@gmail.com>
2023-03-23 18:51:59 -0300

    criado AbstractODM

---
+++ src/Models/AbstractODM.ts
@@ -0,0 +1,19 @@
+import { Model, Schema, model, models } from 'mongoose';
+
+abstract class AbstractODM<T> {
+  protected model: Model<T>;
+  protected modelName: string;
+  protected schema: Schema<T>;
+
+  constructor(modelName: string, schema: Schema<T>) {
+    this.schema = schema;
+    this.modelName = modelName;
+    this.model = models[this.modelName] || model(this.modelName, this.schema);
+  }
+
+  public async create(obj: T): Promise<T> {
+    return this.model.create({ ...obj });
+  }
+}
+
+export default AbstractODM;
