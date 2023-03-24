882e88ed2a6258a6948d9f69e2ce615200373c4d
GabrielRodriguesMachado <gabrm123@gmail.com>
2023-03-24 14:14:02 -0300

    criado metodo delete no CarService

--- src/Services/CarService.ts
+++ src/Services/CarService.ts
@@ -6,6 +6,8 @@ import ErrorClass from '../utils/ErrorClass';
 
 class CarService {
   private _model: CarODM;
+  readonly invalidMongoID: string = 'Invalid mongo id';
+  readonly carNotFound: string = 'Car not found';
 
   constructor(modelODM: CarODM) {
     this._model = modelODM;
@@ -35,19 +37,27 @@ class CarService {
   }
 
   public async findById(id: string): Promise<ICar | null> {
-    if (!isValidObjectId(id)) throw new ErrorClass(422, 'Invalid mongo id');
+    if (!isValidObjectId(id)) throw new ErrorClass(422, this.invalidMongoID);
     const car = await this._model.findById(id);
 
-    if (!car) throw new ErrorClass(404, 'Car not found');
+    if (!car) throw new ErrorClass(404, this.carNotFound);
     return new Car(car).returnData();
   }
 
   public async update(id: string, obj: ICar): Promise<ICar | null> {
-    if (!isValidObjectId(id)) throw new ErrorClass(422, 'Invalid mongo id');
+    if (!isValidObjectId(id)) throw new ErrorClass(422, this.invalidMongoID);
     const car = await this._model.update(id, obj);
 
-    if (!car) throw new ErrorClass(404, 'Car not found');
+    if (!car) throw new ErrorClass(404, this.carNotFound);
     return new Car(car).returnData();
+  }
+
+  public async delete(id: string): Promise<ICar | null> {
+    if (!isValidObjectId(id)) throw new ErrorClass(422, this.invalidMongoID);
+    const car = await this._model.delete(id);
+
+    if (!car) throw new ErrorClass(404, this.carNotFound);
+    return car;
   }
 }
 
