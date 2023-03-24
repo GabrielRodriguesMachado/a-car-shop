b4ad59c072f2247424ba27537427de336128c000
GabrielRodriguesMachado <gabrm123@gmail.com>
2023-03-24 12:34:26 -0300

    criada rota para pesquisa por id

--- src/Routes/CarRoutes.ts
+++ src/Routes/CarRoutes.ts
@@ -15,4 +15,10 @@ carsRouter.get(
     new CarController(req, res, next).findAll(),
 );
 
+carsRouter.get(
+  '/:id',
+  (req: Request, res: Response, next: NextFunction) =>
+    new CarController(req, res, next).findById(),
+);
+
 export default carsRouter;
