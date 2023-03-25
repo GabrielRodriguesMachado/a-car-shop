7a8ef990be76b82cfe9ef0a2dc72115441487840
GabrielRodriguesMachado <gabrm123@gmail.com>
2023-03-25 18:34:34 -0300

    MotorcycleService atualizado com os metodos findAll e findById

--- src/Services/MotorcycleService.ts
+++ src/Services/MotorcycleService.ts
@@ -1,9 +1,13 @@
+import { isValidObjectId } from 'mongoose';
 import Motorcycle from '../Domains/Motorcycle';
 import IMotorcycle from '../Interfaces/IMotorcycle';
 import MotorcycleODM from '../Models/MotorcyleODM';
+import ErrorClass from '../utils/ErrorClass';
 
 class MotorcycleService {
   private _model: MotorcycleODM;
+  readonly invalidMongoID: string = 'Invalid mongo id';
+  readonly motorcycleNotFound: string = 'Motorcycle not found';
 
   constructor(modelODM: MotorcycleODM) {
     this._model = modelODM;
@@ -14,6 +18,30 @@ class MotorcycleService {
     const newMotorcycle = new Motorcycle(result).returnData();
 
     return newMotorcycle;
+  }
+
+  public async findAll(): Promise<IMotorcycle[] | null> {
+    const allMotorcycles = await this._model.findAll();
+    const result = allMotorcycles.map((motorcycle) => ({
+      id: motorcycle.id,
+      model: motorcycle.model,
+      year: motorcycle.year,
+      color: motorcycle.color,
+      status: motorcycle.status,
+      buyValue: motorcycle.buyValue,
+      category: motorcycle.category,
+      engineCapacity: motorcycle.engineCapacity,
+    }));
+
+    return result;
+  }
+
+  public async findById(id: string): Promise<IMotorcycle | null> {
+    if (!isValidObjectId(id)) throw new ErrorClass(422, this.invalidMongoID);
+    const motorcycle = await this._model.findById(id);
+
+    if (!motorcycle) throw new ErrorClass(404, this.motorcycleNotFound);
+    return new Motorcycle(motorcycle).returnData();
   }
 }
 
