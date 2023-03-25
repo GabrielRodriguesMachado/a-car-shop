e002f9f981924dfbbb7830211372fa9434417674
GabrielRodriguesMachado <gabrm123@gmail.com>
2023-03-25 18:20:39 -0300

    criada domain Motorcycle

---
+++ src/Domains/Motorcycle.ts
@@ -0,0 +1,28 @@
+import IMotorcycle from '../Interfaces/IMotorcycle';
+import Vehicle from './Vehicle';
+
+class Motorcycle extends Vehicle {
+  private _category: string;
+  private _engineCapacity: number;
+
+  constructor({ id, model, year, color, status, buyValue, category, engineCapacity }: IMotorcycle) {
+    super({ id, model, year, color, status, buyValue });
+    this._category = category;
+    this._engineCapacity = engineCapacity;
+  }
+
+  public returnData() {
+    return {
+      id: this.id,
+      model: this.model,
+      year: this.year,
+      color: this.color,
+      status: this.status,
+      buyValue: this.buyValue,
+      category: this._category,
+      engineCapacity: this._engineCapacity,
+    };
+  }
+}
+
+export default Motorcycle;
